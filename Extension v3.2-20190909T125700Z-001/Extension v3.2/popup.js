3429669726d8d0a846e21ef235f572fe9e9e63a6
nl1213 <48366157+nl1213@users.noreply.github.com>
2019-09-16 08:08:40 -0400

    Bennett
    Bennett has been added. Bennett now 100% implemented. Oil paints not included.

--- Extension v3.2-20190909T125700Z-001/Extension v3.2/popup.js
+++ Extension v3.2-20190909T125700Z-001/Extension v3.2/popup.js
@@ -17,12 +17,14 @@ document.addEventListener("DOMContentLoaded", function(){
 //  document.getElementById("Howard").addEventListener("click", Howardappear);
 //  document.getElementById("Impact").addEventListener("click", Impactappear);
 //  document.getElementById("Pose").addEventListener("click", Poseappear);
+	document.getElementById("bennett").addEventListener("click",bennettappear);
 });
 /* adds the eventlisteners for all multilink folders. These run the appear function when clicked. Make sure there is a listener for all multilink folders
 Event Listener Template:  document.getElementById("Name").addEventListener("click", Nameappear; 
 */
 
 function Applebyappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Applebysub").style.display="Block";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Chen").style.display="None";
@@ -71,7 +73,62 @@ function Applebyappear(){
 	document.getElementById("Appleby").removeEventListener("click", Applebyappear);
 	document.getElementById("Appleby").addEventListener("click", Applebydisappear)
 }
+
+function bennettappear(){
+	document.getElementById("Appleby").style.display="None";
+	document.getElementById("Site").style.display="None";
+	document.getElementById("Portal").style.display="None";
+	document.getElementById("Chen").style.display="None";
+	document.getElementById("bennett").style.display="None";
+	document.getElementById("Shecalfsub").style.display="None";
+	document.getElementById("Hecalf").style.display="None";
+	document.getElementById("Impact").style.display="None";
+	document.getElementById("Palumbo").style.display="None";
+	document.getElementById("Rauschenberger").style.display="None";
+	document.getElementById("Hughes").style.display="None";
+	document.getElementById("Treen").style.display="None";
+	document.getElementById("Popa").style.display="None";
+//  document.getElementById("Howard").style.display="None";
+	document.getElementById("Griffith").style.display="None";
+//  document.getElementById("Hanna").style.display="None";
+	document.getElementById("Classroom").style.display="None";
+	document.getElementById("Cohen").style.display="None";
+//  document.getElementById("Dreslinski").style.display="None";
+//  document.getElementById("Pose").style.display="None";
+//  document.getElementById("Kaffen").style.display="None";
+	document.getElementById("Graber").style.display="None";
+//  document.getElementById("Cown").style.display="None";
+	document.getElementById("York").style.display="None";
+	document.getElementById("Applebysp").style.display="None";
+	document.getElementById("Sitesp").style.display="None";
+	document.getElementById("Impactsp").style.display="None";
+	document.getElementById("Griffithsp").style.display="None";
+	document.getElementById("Portalsp").style.display="None";
+	document.getElementById("Shecalfsp").style.display="None";
+	document.getElementById("Hecalfsp").style.display="None";
+//  document.getElementById("Howardsp").style.display="None";
+	document.getElementById("Chensp").style.display="None";
+	document.getElementById("Palumbosp").style.display="None";
+	document.getElementById("Rauschenbergersp").style.display="None";
+	document.getElementById("Popasp").style.display="None";
+//  document.getElementById("Hannasp").style.display="None";
+	document.getElementById("Classroomsp").style.display="None";
+	document.getElementById("Hughessp").style.display="None";
+//  document.getElementById("Dreslinskisp").style.display="None";
+//  document.getElementById("Posesp").style.display="None";
+	document.getElementById("Cohensp").style.display="None";
+	document.getElementById("Yorksp").style.display="None";
+//  document.getElementById("Kaffensp").style.display="None";
+//  document.getElementById("Cownsp").style.display="None";
+	document.getElementById("Grabersp").style.display="None";
+	document.getElementById("fillend9").style.display="inline-block";
+	document.getElementById("bennett").removeEventListener("click", bennettappear);
+	document.getElementById("bennett").addEventListener("click", bennettdisappear)
+}
+	
+	
 function Shecalfappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Portal").style.display="None";
@@ -123,6 +180,7 @@ function Shecalfappear(){
 	document.getElementById("Shecalf").addEventListener("click", Shecalfdisappear)
 }
 function Graberappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Chen").style.display="None";
 	document.getElementById("Site").style.display="None";
@@ -171,6 +229,7 @@ function Graberappear(){
 	document.getElementById("Graber").addEventListener("click", Graberdisappear)
 }
 function Hecalfappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Portal").style.display="None";
@@ -270,6 +329,7 @@ function Hecalfappear(){
 	document.getElementById("Hanna").addEventListener("click", Hannadisappear)
 }*/
 function Popaappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Portal").style.display="None";
@@ -318,6 +378,7 @@ function Popaappear(){
 	document.getElementById("Popa").addEventListener("click", Popadisappear);
 }
 function Palumboappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Portal").style.display="None";
@@ -369,6 +430,7 @@ function Palumboappear(){
 	document.getElementById("Palumbo").addEventListener("click", Palumbodisappear);
 }
 function Rauschenbergerappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Portal").style.display="None";
@@ -421,6 +483,7 @@ function Rauschenbergerappear(){
 }
 
 function Chenappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Portal").style.display="None";
@@ -471,6 +534,7 @@ function Chenappear(){
 }
 
 function Griffithappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Portal").style.display="None";
@@ -520,6 +584,7 @@ function Griffithappear(){
 	document.getElementById("Griffith").addEventListener("click", Griffithdisappear)
 }
 function Hughesappear(){
+	document.getElementById("bennett").style.display="None";
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Portal").style.display="None";
@@ -627,7 +692,7 @@ function Hughesappear(){
 function Yorkappear(){
 	document.getElementById("Yorksub").style.display="Block";
 //  document.getElementById("Dreslinski").style.display="None";
-	 
+	document.getElementById("bennett").style.display="None"; 
 	document.getElementById("Appleby").style.display="None";
 	document.getElementById("Site").style.display="None";
 	document.getElementById("Chen").style.display="None";
@@ -872,7 +937,7 @@ function Nameappear(){																These are the links, none of them should b
 	document.getElementById("Impact").style.display="Block";
 	document.getElementById("Cohen").style.display="Block";
 	document.getElementById("Graber").style.display="Block"
-	
+	document.getElementById("bennett").style.display="Block";
 	document.getElementById("Appleby").style.display="Block";
 	document.getElementById("Chen").style.display="Block";
 	document.getElementById("Griffith").style.display="Block";
@@ -909,6 +974,60 @@ function Nameappear(){																These are the links, none of them should b
 	document.getElementById("Kaffen").addEventListener("click", Kaffenappear);
 	document.getElementById("Kaffen").removeEventListener("click", Kaffendisappear);
 }*/
+
+function bennettdisappear(){
+	document.getElementById("Appleby").style.display="Block";
+	document.getElementById("Site").style.display="Block";
+	document.getElementById("Cown").style.display="Block";
+	document.getElementById("Portal").style.display="Block";
+	document.getElementById("Shecalf").style.display="Block";
+	document.getElementById("Hecalf").style.display="Block";
+	document.getElementById("Palumbo").style.display="Block";
+	document.getElementById("Rauschenberger").style.display="Block";
+	document.getElementById("Treen").style.display="Block";
+	document.getElementById("Popa").style.display="Block";
+	document.getElementById("Hanna").style.display="Block";
+	document.getElementById("Pose").style.display="Block";
+	document.getElementById("Classroom").style.display="Block";
+	document.getElementById("Impact").style.display="Block";
+	document.getElementById("Cohen").style.display="Block";
+	document.getElementById("Graber").style.display="Block"
+	document.getElementById("bennett").style.display="Block";
+	document.getElementById("Appleby").style.display="Block";
+	document.getElementById("Chen").style.display="Block";
+	document.getElementById("Griffith").style.display="Block";
+	document.getElementById("Hughes").style.display="Block";
+	
+	document.getElementById("York").style.display="Block";
+//	document.getElementById("Kaffensub").style.display="None";
+//	document.getElementById("Dreslinski").style.display="Block";
+	
+	document.getElementById("Applebysp").style.display="inline-block";
+	document.getElementById("Sitesp").style.display="inline-block";
+	document.getElementById("Chensp").style.display="inline-block";
+	document.getElementById("Yorksp").style.display="inline-block";
+	document.getElementById("Impact").style.display="inline-block";
+	document.getElementById("Griffithsp").style.display="inline-block";
+//	document.getElementById("Kaffensp").style.display="inline-block";
+	document.getElementById("Hughessp").style.display="inline-block";
+	document.getElementById("Portalsp").style.display="inline-block";
+//	document.getElementById("Cownsp").style.display="inline-block";
+	document.getElementById("Shecalfsp").style.display="inline-block";
+	document.getElementById("Posesp").style.display="inline-block";
+	document.getElementById("Hecalfsp").style.display="inline-block";
+	document.getElementById("Palumbosp").style.display="inline-block";
+	document.getElementById("Rauschenbergersp").style.display="inline-block";
+	document.getElementById("Popasp").style.display="inline-block";
+//	document.getElementById("Dreslinskisp").style.display="inline-block";
+//	document.getElementById("Hannasp").style.display="inline-block";
+	
+	document.getElementById("Classroomsp").style.display="inline-block";
+	document.getElementById("Cohensp").style.display="inline-block";
+	document.getElementById("Grabersp").style.display="inline-block";
+	document.getElementById("York").addEventListener("click", bennettappear);
+	document.getElementById("York").removeEventListener("click",bennettdisappear);
+	document.getElementById("fillend2").style.display="None";
+}
 function Yorkdisappear(){
 	document.getElementById("Appleby").style.display="Block";
 	document.getElementById("Site").style.display="Block";
